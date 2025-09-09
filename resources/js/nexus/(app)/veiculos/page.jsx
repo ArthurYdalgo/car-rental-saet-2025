9839abb1fa70514573c88555c3aad0eb2f903fd0
Arthur Ydalgo <25585428+ArthurYdalgo@users.noreply.github.com>
2025-09-08 22:09:48 -0300

    up

--- resources/js/nexus/(app)/veiculos/page.jsx
+++ resources/js/nexus/(app)/veiculos/page.jsx
@@ -1,9 +1,13 @@
+import MomentDateTime from '@/components/moment-date-time';
+import Money from '@/components/Money';
+import Number from '@/components/Number';
 import Table from '@/components/pagination/table';
+import TableSortableField from '@/components/pagination/table-sortable-field';
+import { TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/app-layout';
+import { Tab } from '@headlessui/react';
 import { Head } from '@laravext/react';
 import { useState } from 'react';
-import { TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import MomentDateTime from '@/components/moment-date-time';
 
 const breadcrumbs = [
     {
@@ -26,7 +30,27 @@ export default function Dashboard() {
                         <TableHeader>
                             <TableRow>
                                 <TableHead>ID</TableHead>
-                                <TableHead>Criado em</TableHead>
+                                <TableHead>Nome</TableHead>
+                                <TableHead>
+                                    <TableSortableField handleClick={handleClick} sortBy={sortBy} field={'year'}>
+                                        Ano
+                                    </TableSortableField>
+                                </TableHead>
+                                <TableHead>
+                                    <TableSortableField handleClick={handleClick} sortBy={sortBy} field={'seats'}>
+                                        Lugares
+                                    </TableSortableField>
+                                </TableHead>
+                                <TableHead>
+                                    <TableSortableField handleClick={handleClick} sortBy={sortBy} field={'trunk_capacity'}>
+                                    Volume do Porta-malas
+                                    </TableSortableField>
+                                    </TableHead>
+                                <TableHead>
+                                    <TableSortableField handleClick={handleClick} sortBy={sortBy} field={'price_per_day'}>
+                                    Preço por Dia
+                                    </TableSortableField>
+                                    </TableHead>
                             </TableRow>
                         </TableHeader>
                     )}
@@ -36,7 +60,15 @@ export default function Dashboard() {
                                 {pagination.data.map((vehicle) => (
                                     <TableRow key={vehicle.id}>
                                         <TableCell>{vehicle.id}</TableCell>
-                                        <TableCell><MomentDateTime date={vehicle.created_at} /></TableCell>
+                                        <TableCell>{vehicle.name}</TableCell>
+                                        <TableCell>{vehicle.year}</TableCell>
+                                        <TableCell>{vehicle.seats}</TableCell>
+                                        <TableCell>
+                                            <Number value={vehicle.trunk_capacity} maximumFractionDigits={0} minimumFractionDigits={0} sufix={'L'} />
+                                        </TableCell>
+                                        <TableCell>
+                                            <Money amount={vehicle.price_per_day} />
+                                        </TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
