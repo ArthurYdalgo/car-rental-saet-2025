e66c46d2eaab82b806c6309025f8648c743f916f
Arthur Ydalgo <25585428+ArthurYdalgo@users.noreply.github.com>
2025-10-17 15:56:02 -0300

    up

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'   => ['*'],
-    'allowed_origins' => [env('APP_URL'), env('FRONTEND_URL', 'http://localhost:3000'), 'localhost:3000', 'https://localhost:3000'],
+    'allowed_origins' => [env('APP_URL'), 'localhost', env('FRONTEND_URL', 'http://localhost:3000'), 'localhost:3000', 'https://localhost:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers'   => ['*'],
     'exposed_headers'   => [],
