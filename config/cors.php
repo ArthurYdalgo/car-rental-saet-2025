05645df4b8efa48b97ac6d2cf182638075e3aa54
Arthur Ydalgo <25585428+ArthurYdalgo@users.noreply.github.com>
2025-10-17 15:32:31 -0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'   => ['*'],
-    'allowed_origins'   => [
-        'https://car-rental-saet-2025-main-rfctpl.laravel.cloud',
-        'http://localhost:3000',
-    ],
+    'allowed_origins' => [env('APP_URL'), env('FRONTEND_URL', 'http://localhost:3000'), 'localhost:3000', 'https://localhost:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers'   => ['*'],
     'exposed_headers'   => [],
